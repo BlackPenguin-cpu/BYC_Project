22f1eed97289ce6c357fad03feec35bf6ceedf48
Black_Penguin <dkstmdwnswns@gmail.com>
2022-12-14 18:45:08 +0900

    reImport

---
+++ Community/Assets/Scripts/UIManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnBtnClick()
+    {
+
+    }
+}
